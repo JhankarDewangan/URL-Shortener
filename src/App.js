aa978011019f40d02ea9c87b148fc2103134800c
Jhankar Dewangan <117924287+JhankarDewangan@users.noreply.github.com>
2023-03-03 00:02:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+import Header from "./Header";
+import Middle from "./Middle";
+import Footer from "./Footer";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Middle />
+      <Footer />
+    </div>
+  );
+}
